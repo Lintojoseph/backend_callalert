da6f11b8070e50120694d0cda5da15f0b8b90349
unknown <lintojoseph2097@gmail.com>
2025-06-07 14:56:41 +0530

    second commit

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL:  process.env.GOOGLE_CALLBACK_URL,
     scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar.readonly'],
     passReqToCallback: true
   },
